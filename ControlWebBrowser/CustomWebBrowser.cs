02ed177471305a562adbeb49cc2b4784cfb5d955
岩橋亮人 <redia0911@live.com>
2018-12-27 23:44:49 +0900

    Release_BetaVer1.4.0._20181227

--- ControlWebBrowser/CustomWebBrowser.cs
+++ ControlWebBrowser/CustomWebBrowser.cs
@@ -65,6 +65,17 @@ namespace ControlWebBrowser
             chkGood.Checked = true;
             chkViewCount.Checked = true;
             txtViewCount.Text = "10";
+            ChangeUserAgent();
+        }
+        [DllImport("urlmon.dll", CharSet = CharSet.Ansi)]
+        private static extern int UrlMkSetSessionOption(int dwOption, string pBuffer, int dwBufferLength, int dwReserved);
+        const int URLMON_OPTION_USERAGENT = 0x10000001;
+        public void ChangeUserAgent()
+        {
+            List<string> userAgent = new List<string>();
+            string ua = "Googlebot/2.1 (+http://www.google.com/bot.html)";
+
+            UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, ua, ua.Length, 0);
         }
         private void btnStart_Click(object sender, EventArgs e)
         {
